a5937e5076a2fc72154c4f8b92a2048c24b1d780
Peter Thomas <peter@Peters-MacBook-Pro.local>
2021-06-22 15:00:26 -0400

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,35 +8,38 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-/*
- * infinite_while
- * 	takes no parameters
- * 	performs an infinite loop
+/**
+ * infinite_while - Infinite loop
+ * @void: void
+ *
+ * Description: Loops infinitely
+ * Return: 0
  */
-
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
-/*
- * main()
- * Program entry point
+/**
+ * main - Program entry point
+ * @void: void
+ *
+ * Description: Program entry point
+ * Return: 0
  */
-
 int main(void)
 {
 	pid_t child;
 
-	for (int i=0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		child = fork();
 
-		if(child)
+		if (child)
 			printf("Zombie process created, PID: %d\n", child);
 		else
 			exit(0);
@@ -44,5 +47,5 @@ int main(void)
 
 	infinite_while();
 
-	return 0;
+	return (0);
 }
