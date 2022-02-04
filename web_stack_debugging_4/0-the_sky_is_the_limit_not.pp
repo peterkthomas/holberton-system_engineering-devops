3e2ab899c64bbefd2b4c49773a7c66322f85e804
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2022-02-03 23:45:28 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,4 +2,10 @@
 exec { 'fix nginx ulimit':
   command => 'sed -i "s/15/$( ulimit -n )/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
+} ->
+
+# next restart nginx
+exec { 'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/bin/'
 }
